262d3330e92394bf09d504126f29461595a27f35
ZeroMin-K <ymkim9510@gmail.com>
2022-07-27 21:29:37 +0900

    Daily Coding 문제 풀이
    Q.06 Letter Capitalize

--- dailyCodingJava/Q06LetterCapitalize.java
+++ dailyCodingJava/Q06LetterCapitalize.java
@@ -17,14 +17,19 @@ public class Q06LetterCapitalize {
             return "";                      // 빈 문자열 리턴
         }
 
+        // 공백을 구분으로 문자열 나누기
         String[] words = str.split(" ");
         System.out.println("단어별로 split: " + Arrays.toString(words));
         String result = "";
 
+        // 나눈 문자열 순회
         for (int i = 0; i < words.length; i++) {
+            // 공백이면
             if (words[i].isEmpty()) {
+                // 공백
                 words[i] = words[i];
             } else {
+                // 단어이면 - 첫글자는 대문자로
                 words[i] = String.valueOf(words[i].charAt(0)).toUpperCase() + words[i].substring(1);
             }
         }
